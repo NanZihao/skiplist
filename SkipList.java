ad2a72c479e1ae08ce5284ca143e5db78af713f7
qin <yinfeng.qin@gmail.com>
2013-01-05 16:11:27 -0500

    Added empty file.

---
+++ SkipList.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class SkipList {
+  public SkipList() {
+  }
+
+  public static void main(String[] args) {
+    SkipList skipList = new SkipList();
+
+    System.out.println(skipList.toString());
+  }
+}
